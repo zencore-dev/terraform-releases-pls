eba4c388e8e12adefef3a4c7b9ee777440a1fe62
Felipe Valdivia Vivar <fvaldivia@zencored.dev>
2023-09-07 11:49:18 -0300

    first commit for PR Module

---
+++ modules/project/main.tf
@@ -0,0 +1,7 @@
+module "project-factory" {
+  source          = "terraform-google-modules/project-factory/google"
+  version         = "14.3.0"
+  name            = var.project_name
+  billing_account = var.billing_account
+  org_id          = var.org_id
+}
---
+++ modules/project/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "org_id" {
+  type        = string
+  description = "The organization id"
+}
+
+variable "billing_account" {
+  type        = string
+  description = "The billing account id"
+}
