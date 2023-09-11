4566e5a8f9291b44479dcada108b93185ddb1a03
Felipe Valdivia Vivar <fvaldivia@zencored.dev>
2023-09-11 15:53:05 -0300

    test

--- modules/project/versions.tf
+++ modules/project/versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_version = ">=0.13.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 4.28"
     }
+
     google-beta = {
       source  = "hashicorp/google-beta"
       version = "~> 4.28"
