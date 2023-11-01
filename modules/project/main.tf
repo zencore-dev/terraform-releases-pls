e6ceafe1133465e189bf880f37a1a635a7928b2b
matthew walter <matthew.walter@equilibriumenergy.com>
2023-11-01 07:49:51 -0400

    fix: test release

--- modules/project/main.tf
+++ modules/project/main.tf
@@ -1,8 +1,8 @@
 module "project-factory" {
-  # simple change, test CI
   source          = "terraform-google-modules/project-factory/google"
   version         = "14.3.0"
   name            = var.project_name
   billing_account = var.billing_account
   org_id          = var.org_id_number
+  # change here now
 }
